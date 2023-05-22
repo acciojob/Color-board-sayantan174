8504df11105173a9904dda865eb91d291b70148a
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-05-22 20:53:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const container = document.getElementById("container");
+for (let i = 0; i < 20; i++) {
+  for (let j = 0; j < 40; j++) {
+    const div = document.createElement("div");
+    div.className = "square";
+    container.appendChild(div);
+  }
+  const br = document.createElement("br");
+  container.appendChild(br);
+}
